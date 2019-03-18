039fd2b8dffd86935dd58693cb21530362933af1
harshaathnere <harshaathnere@gmail.com>
2019-03-18 16:08:15 +0530

    added removed function

--- dom.js
+++ dom.js
@@ -1,34 +1,69 @@
-//PRIMITIVE DATA TYPE
-//string
-const  name = 'Harsha';
-//number
-const age = 45;
-console.log(typeof name);
-console.log(typeof age);
-//boolean
-const haskids = true;
-console.log(typeof haskids);
-// null
-const car = null;
-console.log(typeof car);
- // undefined
- let test;
+// //PRIMITIVE DATA TYPE
+// //string
+// const  name = 'Harsha';
+// //number
+// const age = 45;
+// console.log(typeof name);
+// console.log(typeof age);
+// //boolean
+// const haskids = true;
+// console.log(typeof haskids);
+// // null
+// const car = null;
+// console.log(typeof car);
+//  // undefined
+//  let test;
 
- //symbol
- const sym = Symbol()
- console.log(typeof sym);
+//  //symbol
+//  const sym = Symbol()
+//  console.log(typeof sym);
 
-//  REFERENCE DATA TYPE
-// Array
- const hobbies = ['movie','music','dance'];
- console.log(typeof sym);
- // object literal
- const address = {
-     city:  'bhopal',
-     state: 'mp'
- }
- console.log(typeof address);
- //date
- const today = new Date();
- console.log(today);
- console.log(typeof today);
+// //  REFERENCE DATA TYPE
+// // Array
+//  const hobbies = ['movie','music','dance'];
+//  console.log(typeof sym);
+//  // object literal
+//  const address = {
+//      city:  'bhopal',
+//      state: 'mp'
+//  }
+//  console.log(typeof address);
+//  //date
+//  const today = new Date();
+//  console.log(today);
+//  console.log(typeof today);
+
+let val;
+// Number to string
+val = String(555) ;
+val = String(4+4);
+// Bool to string
+val = String(true)
+ // date to string
+
+ val = String(new Date());
+ //Array to string
+ val = String([1,2,3,4]);
+ //to string 
+ val = (5).toString();
+ val = (true).toString();
+ // string to number
+ val = Number('5');
+ val = Number(true);
+ val = Number(false);
+ val = Number(null);
+ val = Number('hello');
+ val = Number([1,2,3,4]);
+  val = parseInt('1000');
+  val = parseFloat('100.38');
+
+//output
+// console.log(val);
+// console.log(typeof val);
+// //console.log(val.length);
+// console.log(val.toFixed());
+ const val1 = String(5);
+ const val2 = 6;
+ const sum = Number(val1 + val2);
+ console.log(sum);
+ console.log(typeof sum);
