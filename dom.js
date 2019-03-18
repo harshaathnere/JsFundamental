a6cbb2bd92064c93dba58ce70356da98c29608cf
harshaathnere <harshaathnere@gmail.com>
2019-03-18 14:52:45 +0530

    My very first commit

---
+++ dom.js
@@ -0,0 +1,34 @@
+//PRIMITIVE DATA TYPE
+//string
+const  name = 'Harsha';
+//number
+const age = 45;
+console.log(typeof name);
+console.log(typeof age);
+//boolean
+const haskids = true;
+console.log(typeof haskids);
+// null
+const car = null;
+console.log(typeof car);
+ // undefined
+ let test;
+
+ //symbol
+ const sym = Symbol()
+ console.log(typeof sym);
+
+//  REFERENCE DATA TYPE
+// Array
+ const hobbies = ['movie','music','dance'];
+ console.log(typeof sym);
+ // object literal
+ const address = {
+     city:  'bhopal',
+     state: 'mp'
+ }
+ console.log(typeof address);
+ //date
+ const today = new Date();
+ console.log(today);
+ console.log(typeof today);
